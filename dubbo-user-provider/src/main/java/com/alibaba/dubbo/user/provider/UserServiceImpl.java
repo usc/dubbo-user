6f6f4bee4b7661cd4c54e0896f4412ed79fea179
lishunli <lishunli.me@gmail.com>
2015-06-20 15:12:35 +0800

    support XMLConfiguration

--- dubbo-user-provider/src/main/java/com/alibaba/dubbo/user/provider/UserServiceImpl.java
+++ dubbo-user-provider/src/main/java/com/alibaba/dubbo/user/provider/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.alibaba.dubbo.user.provider;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.configuration.XMLConfiguration;
+
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.dubbo.user.api.User;
 import com.alibaba.dubbo.user.api.UserService;
@@ -10,9 +14,14 @@ import com.alibaba.dubbo.user.api.UserService;
  */
 public class UserServiceImpl implements UserService {
 
+    @Resource(name = "userConfig")
+    private XMLConfiguration config;
+
     @Override
     public User getUser(String id) {
         System.out.println(RpcContext.getContext().getLocalAddressString() + " UserService.getUser() invoked");
+
+        System.out.println(config.getString("db.name"));
 
         User user = new User(id, "name" + id);
         return user;
